befc8407c5f4b16dcfb008d6be408841d8f23824
Albert Unruh <keenan.noack@gmail.com>
2021-02-09 14:25:35 +0100

    debugged the alias

--- Modules/ping.py
+++ Modules/ping.py
@@ -4,8 +4,8 @@ import Utils
 
 HELP = Utils.Help("shows the ping")
 EVENTS = [Utils.EVENT.on_message]
-ALIASES = [":ping_pong:"]
+ALIASES = ["🏓"]
 
 
 async def __main__(client: discord.Client, _event: int, message: discord.Message):
-    await message.channel.send(f"Pong :ping_pong:\nI have a latency from {round(client.latency, 2)} seconds")
+    await message.channel.send(f"Pong 🏓\nI have a latency from {round(client.latency, 2)} seconds")
