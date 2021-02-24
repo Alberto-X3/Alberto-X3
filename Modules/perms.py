9417895e48308853bc3842dbba8bb56d38037e2e
Albert Unruh <keenan.noack@gmail.com>
2021-02-24 23:14:09 +0100

    updated `perms.py` for the new update (now can manage the permissions)

--- Modules/perms.py
+++ Modules/perms.py
@@ -5,15 +5,14 @@ import Utils
 HELP = Utils.Help(f"shows you your permissions", f"""
 > *{Utils.Prefix}perms*
 shows you your perms
-_requires _
 
 > *{Utils.Prefix}perms [USER-ID|USER-MENTION]*
 shows you perms from other
 
-> *{Utils.Prefix}perms [add|+] [USER-ID|USER-MENTION] [PERMISSION]*
+> *{Utils.Prefix}perms [USER-ID|USER-MENTION] [add|+] [PERMISSION]*
 adds permissions for a user
 
-> *{Utils.Prefix}perms [remove|-] [USER-ID|USER-MENTION] [PERMISSION]*
+> *{Utils.Prefix}perms [USER-ID|USER-MENTION] [remove|-] [PERMISSION]*
 removes permissions from a user
 
 """)
@@ -28,7 +27,7 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
     # not 'seeOwn'
     if len(message.content.split()) > 1:
 
-        if message.content.split()[1].replace("<", "").replace("@", "").replace("!", "").replace(">", "").isnumeric():
+        if message.content.split()[1].replace("<", "").replace("@", "").replace("!", "").replace(">", "").isnumeric() and len(message.content.split()) == 2:
 
             # 'seeOther'
             if len(message.content.split()) == 2:
@@ -41,13 +40,13 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
 
         # 'set'
         elif len(message.content.split()) == 4:
-            if message.content.split()[2].replace("<", "").replace("@", "").replace("!", "").replace(">", "").isnumeric():
+            if message.content.split()[1].replace("<", "").replace("@", "").replace("!", "").replace(">", "").isnumeric():
 
-                if message.content.split()[1] == "add" or message.content.split()[1] == "+":
-                    await add(message, user_perms)
+                if message.content.split()[2] == "add" or message.content.split()[2] == "+":
+                    await set_(message, user_perms, True)
 
-                elif message.content.split()[1] == "remove" or message.content.split()[1] == "-":
-                    await remove(message, user_perms)
+                elif message.content.split()[2] == "remove" or message.content.split()[2] == "-":
+                    await set_(message, user_perms, False)
 
             else:
                 await message.channel.send(":x: Please enter a ID or a MENTION")
@@ -80,17 +79,62 @@ async def perms(message: discord.Message, _id: str, user_perms: Utils.AttrDict):
     await message.channel.send(f"**__perms from <@{_id}>:__**\n{log}")
 
 
-async def add(message: discord.Message, user_perms: Utils.AttrDict):
-    if user_perms.User.Perms.set:
-        await message.channel.send(":x: Not implemented yet :x:")
-
+def replace(root: Utils.Union[Utils.AttrDict, bool], keys: list, value: Utils.Any):
+    if not keys:
+        return value
     else:
-        await message.channel.send(":x: requires User.Perms.set")
+        root[keys[0]] = replace(root[keys[0]], keys[1:], value)
+        return root
 
 
-async def remove(message: discord.Message, use_perms: Utils.AttrDict):
-    if use_perms.User.Perms.set:
-        await message.channel.send(":x: Not implemented yet :x:")
+def replaces(root: Utils.Union[Utils.AttrDict, bool], value: Utils.Optional[bool]):
+    for key in root:
+        if isinstance(root[key], bool):
+            root[key] = value
+        else:
+            root[key] = replaces(root[key], value)
+    return root
+
+
+async def set_(message: discord.Message, user_perms: Utils.AttrDict, new: bool):
+    if user_perms.User.Perms.set:
+
+        try:
+            perm = eval(f"user_perms.{message.content.split()[-1]}")
+        except KeyError:
+            await message.channel.send(":x: This permission doesn't exist! :x:")
+        else:
+            id_ = message.content.split()[1].replace("<", "").replace("@", "").replace("!", "").replace(">", "")
+            if id_ == str(message.author.id):
+                await message.channel.send("**__:x: Please don't modify yourself! :x:__**\nhere is a 🃏 for you \\:)")
+                return
+
+            root = Utils.perms(id_)
+            keys = message.content.split()[-1].split('.')
+            if isinstance(perm, bool):
+                if perm:
+                    new_perms = replace(root, keys, new)
+
+                else:
+                    await message.channel.send(":x: You don't have this permission! :x:")
+                    return
+            else:
+                if new is False:
+                    if user_perms.User.Perms.removeMultiple:
+                        new_perms = replace(root, keys, replaces(eval(f"root.{message.content.split()[-1]}"), new))
+                    else:
+                        await message.channel.send(":x: You don't have permissions to remove multiple permissions at once! :x:")
+                        return
+                else:
+                    await message.channel.send(":x: You don't have permissions to add multiple permissions at once! :x:")
+                    return
+
+            from json import load, dump
+            permissions = Utils.AttrDict(load(open("perms.json")))
+            permissions[id_] = new_perms
+            dump(permissions, open("perms.json", "w"), indent=2)
+
+            await message.channel.send(f"**Successfully {'added permissions to' if new is True else 'removed permissions from'} <@{id_}>**")
 
     else:
         await message.channel.send(":x: requires User.Perms.set")
