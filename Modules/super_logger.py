5263db1d8754cec658a4466df23956afbd82bb6e
Albert Unruh <keenan.noack@gmail.com>
2021-03-17 23:36:25 +0100

    now removed a exception if a message is dm

--- Modules/super_logger.py
+++ Modules/super_logger.py
@@ -24,9 +24,9 @@ async def __main__(client: Client, _event: int, *args: Union[Message, Member, Vo
 
             if args[0].channel.id != super_log.id:
                 embed: Embed = Embed(title=f"on_message | "
-                                           f"<{args[0].jump_url}> |"
-                                           f" {args[0].channel.category} |"
-                                           f" {args[0].channel.mention}",
+                                           f"<{args[0].jump_url}> |" +
+                                           (f" {args[0].channel.category} |" if hasattr(args[0].channel, "category") else " DM |")
+                                           + f" {args[0].channel.mention}",
                                      description=args[0].content+(" | EMBED" if args[0].embeds else "")+(" | ATTACHMENT" if args[0].attachments else ""),
                                      color=Color.gold())
                 embed.set_author(name=args[0].author, url=args[0].author.avatar_url)
