a1024a60b7c8a86a4f69adce74e0810310d8b40c
Albert Unruh <keenan.noack@gmail.com>
2021-03-17 23:27:59 +0100

    removed a exception if a message is dm

--- Modules/super_logger.py
+++ Modules/super_logger.py
@@ -46,9 +46,9 @@ async def __main__(client: Client, _event: int, *args: Union[Message, Member, Vo
         elif _event == EVENT.on_message_delete:
             datetime_edit = False
             embed: Embed = Embed(title=f"on_message_delete | "
-                                       f"<{args[0].jump_url}> |"
-                                       f" {args[0].channel.category} |"
-                                       f" {args[0].channel.mention}",
+                                       f"<{args[0].jump_url}> |" +
+                                       (f" {args[0].channel.category} |" if hasattr(args[0].channel, "category") else " DM |")
+                                       + f" {args[0].channel.mention}",
                                  description=args[0].content+(" | EMBED" if args[0].embeds else "")+(" | ATTACHMENT" if args[0].attachments else ""),
                                  color=Color.gold())
             embed.set_author(name=args[0].author, url=args[0].author.avatar_url)
@@ -59,9 +59,9 @@ async def __main__(client: Client, _event: int, *args: Union[Message, Member, Vo
             datetime_edit = False
             if args[0].author.id != client.user.id:
                 embed: Embed = Embed(title=f"on_message_edit | "
-                                           f"<{args[0].jump_url}> |"
-                                           f" {args[0].channel.category} |"
-                                           f" {args[0].channel.mention}",
+                                           f"<{args[0].jump_url}> |" +
+                                           (f" {args[0].channel.category} |" if hasattr(args[0].channel, "category") else " DM |")
+                                           + f" {args[0].channel.mention}",
                                      color=Color.gold())
                 embed.set_author(name=args[0].author, url=args[0].author.avatar_url)
                 embed.add_field(name=f"before ({args[0].created_at})",
