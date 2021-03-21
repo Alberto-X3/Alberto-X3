1cf3f5631e111d927c0cd21778de6ed5786f6fde
Albert Unruh <keenan.noack@gmail.com>
2021-03-21 20:54:45 +0100

    fixed if-statements

--- Modules/super_logger.py
+++ Modules/super_logger.py
@@ -126,13 +126,13 @@ async def __main__(client: Client, _event: int, *args: Union[Message, Member, Vo
                 if len(args[0].roles) < len(args[1].roles):
                     meta = list()
                     for role in args[1].roles:
-                        if role in args[0].roles:
+                        if role not in args[0].roles:
                             meta.append(f"**+** `{role.name}` `{role.id}`")
                     embed.add_field(name="role", value="\n".join(meta))
                 if len(args[0].roles) > len(args[1].roles):
                     meta = list()
                     for role in args[1].roles:
-                        if role in args[0].roles:
+                        if role not in args[0].roles:
                             meta.append(f"**+** `{role.name}` `{role.id}`")
                     embed.add_field(name="role", value="\n".join(meta))
                 if len(args[0].roles) == len(args[1].roles):
