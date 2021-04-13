156f514e8cd3cfa934e3ecb00ab3cd72f25735ca
Albert Unruh <keenan.noack@gmail.com>
2021-04-14 00:00:49 +0200

    shows you now your global rank

--- Modules/level.py
+++ Modules/level.py
@@ -60,7 +60,11 @@ async def __main__(client: Client, _event: int, message: Message):
             bar = bar.replace("#", full)
             bar = bar.replace("-", free)
 
-            embed: Embed = Embed(color=0x275591)
+            cursor.execute(f"SELECT level FROM lvl where xp>{xp}")
+            rank = len(cursor.fetchall()) + 1
+
+            embed = Embed(color=0x275591,
+                          description=f"You are the number __**#{rank}**__!")
             embed.set_author(name=message.author.name,
                              icon_url=message.author.avatar_url)
 
