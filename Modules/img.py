f46af6d189d83420ac9577eb57f496f0e61887b4
Albert Unruh <keenan.noack@gmail.com>
2021-04-13 14:13:58 +0200

    renamed the cmd

--- Modules/img.py
+++ Modules/img.py
@@ -4,7 +4,7 @@ import Utils
 
 HELP = Utils.Help("lists all profiles", "shows you the history from all profiles of the Bot and the creator")
 EVENTS = [Utils.EVENT.on_message]
-ALIASES = ["img", "profile", "profiles"]
+ALIASES = ["imgs", "profile", "profiles"]
 
 _00 = "https://media.discordapp.net/attachments/779620254396579850/831499354270335006/00-Alberto-X3_ft.Albert.png"
 _01 = "https://media.discordapp.net/attachments/779620254396579850/831501108731314196/01-Alberto-X3_ft.Dr.Negativ.png"
@@ -15,11 +15,9 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
     try:
         await message.channel.send(embed=discord.Embed(description=f"""
     __History:__
-    
-    • [__00__]({_00}) by <@546320163276849162>
-    • [__01__]({_01}) by <@665288034274639873>
-    • [__02__]({_02}) by <@665288034274639873>
-
+    • [__`00`__]({_00}) by <@546320163276849162>
+    • [__`01`__]({_01}) by <@665288034274639873>
+    • [__`02`__]({_02}) by <@665288034274639873>
     """))
 
     except Exception as e:
