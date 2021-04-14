9b27eb9afdf816bb063093e923dea1ba30e5fabf
Albert Unruh <keenan.noack@gmail.com>
2021-04-14 22:00:53 +0200

    better Exception-Handling

--- Modules/unban.py
+++ Modules/unban.py
@@ -25,7 +25,11 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
                     logger = Utils.Logger(channel=await client.fetch_channel(DATA.IDs.Channels.Logs))
                     channel: discord.TextChannel = message.channel
                     guild:         discord.Guild = message.guild
-                    user:           discord.User = await client.fetch_user(int(message.content.split()[1]))
+                    try:
+                        user: discord.User = await client.fetch_user(int(message.content.split()[1]))
+                    except discord.NotFound:
+                        await channel.send(":x: Invalid User-iD :x:")
+                        return
                     handler:        discord.User = message.author
 
                     try:
@@ -35,6 +39,8 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
                         await message.delete()
                     except ValueError:
                         await channel.send(":x: ERROR :x:")
+                    except discord.NotFound:
+                        await channel.send(":x: This user isn't banned :x:")
                     except discord.Forbidden:
                         pass
 
