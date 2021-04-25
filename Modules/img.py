9e265cabe71f6da8bd6fda4ad4f0073ed9a1ae24
Albert Unruh <keenan.noack@gmail.com>
2021-04-25 22:22:25 +0200

    new IMG!!!

--- Modules/img.py
+++ Modules/img.py
@@ -9,6 +9,7 @@ ALIASES = ["imgs", "profile", "profiles"]
 _00 = "https://media.discordapp.net/attachments/779620254396579850/831499354270335006/00-Alberto-X3_ft.Albert.png"
 _01 = "https://media.discordapp.net/attachments/779620254396579850/831501108731314196/01-Alberto-X3_ft.Dr.Negativ.png"
 _02 = "https://media.discordapp.net/attachments/779620254396579850/831501111676239882/02-Alberto-X3_ft.Dr.Negativ.png"
+_03 = "https://media.discordapp.net/attachments/779620254396579850/835973105251647508/03-Alberto-X3_ft.Dr.Negativ.png"
 
 
 async def __main__(client: discord.Client, _event: int, message: discord.Message):
@@ -18,6 +19,7 @@ async def __main__(client: discord.Client, _event: int, message: discord.Message
     • [__`00`__]({_00}) by <@546320163276849162>
     • [__`01`__]({_01}) by <@665288034274639873>
     • [__`02`__]({_02}) by <@665288034274639873>
+    • [__`03`__]({_03}) by <@665288034274639873>
     """))
 
     except Exception as e:
