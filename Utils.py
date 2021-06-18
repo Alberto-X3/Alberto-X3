b13bd8cfb55ca731dcea132786705c7f397bed75
Albert Unruh <keenan.noack@gmail.com>
2021-06-18 19:27:33 +0200

    added a filename for the exception-log-file

--- Utils.py
+++ Utils.py
@@ -228,10 +228,13 @@ def perms(_id: str) -> AttrDict:
 
 
 async def send_exception(client: discord.Client, exception: Exception, source_name: str, mention_role: Optional[int] = 820974562770550816, pin: bool = True, timestamp: bool = True):
+    """
+    sends the exception into a channel if `DATA.debug` ist disabled
+    """
     if not DATA.debug:
         super_log: discord.TextChannel = client.get_channel(DATA.IDs.Channels.Super_Log)
 
-        file = discord.File(BytesIO(format_exc().encode()))
+        file = discord.File(BytesIO(format_exc().encode()), "exception.log")
         embed: discord.Embed = discord.Embed(title=source_name,
                                              description=f"{exception.__class__.__name__}: {exception.__str__()}\n",
                                              color=discord.Color.magenta())
