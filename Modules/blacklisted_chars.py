5a299424f5dfd1c859ae3cb66a8341d4d46e6421
Albert Unruh <keenan.noack@gmail.com>
2021-07-05 16:39:03 +0200

    (: [updated]

--- Modules/blacklisted_chars.py
+++ Modules/blacklisted_chars.py
@@ -1,5 +1,5 @@
 """
-deletes all messages with blacklisted content
+deletes all messages with blacklisted content only
 """
 from discord import Message, Client
 from Utils import Help, EVENT, send_exception
@@ -12,6 +12,7 @@ BLACKLISTED = (
     "\u200b",
     "\u200c",
     "\u200d",
+    " "
 )
 
 
@@ -20,11 +21,10 @@ async def __main__(client: Client, _event: int, message: Message):
         if message.author.id == client.user.id:
             return
 
-        c = message.content or ""
-        for b in BLACKLISTED:
-            if b in c:
-                await message.delete()
-                break
+        for c in message.content or "":
+            if c not in BLACKLISTED:
+                return
+        await message.delete()
 
     except Exception as e:
         await send_exception(client=client, exception=e, source_name=__name__)
